b9f44336eee526fd88f20f91d977c97847c1d3c9
B3Kay <benjaminjekarlsson@gmail.com>
2024-10-10 13:20:24 +0200

    Refactor root page to get companies on server and show simple company card

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,42 @@
-"use client";
-
 import { Inter } from "@next/font/google";
-import { useEffect, useState } from "react";
+import { Company } from "../services/companies/types";
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-  const [stuff1, setStuff1] = useState<any>([]);
-  useEffect(() => {
-    // declare the data fetching function
-    const fetchData = async () => {
-      const data = await fetch("/api/companies");
-      const data2 = await data.json();
-      console.log(data2);
-      setStuff1(data2);
-    };
 
-    // call the function
-    fetchData()
-      // make sure to catch any error
-      .catch(console.error);
-  }, []);
+async function fetchCompanies() {
+  const res = await fetch("http://localhost:3000/api/companies");
+  if (!res.ok) {
+    console.warn("Unexpected response status:", res.status);
+    throw new Error("Failed to fetch data");
+  }
+  const data = await res.json();
+  return data.data as Company[];
+}
+
+
+interface CompanyCardProps {
+  company: Company;
+}
+
+const CompanyCard: React.FC<CompanyCardProps> = ({ company }) => {
+  return (
+    <div className="company-card">
+      <img style={{ maxHeight: "64px", width: "auto" }} src={company.logoLightUrl} alt={`${company.companyName} logo`} />
+      <h3>{company.displayName}</h3>
+      <p>{company.description}</p>
+    </div>
+  );
+};
+
+
+export default async function Home() {
+  const companies = await fetchCompanies();
 
   return (
     <main>
       <h2 className={inter.className}>Quartr</h2>
       <p className={inter.className}>Trending companies</p>
-      <p>{JSON.stringify(stuff1)}</p>
+      <div>{companies.map((company) => <CompanyCard key={company.companyId} company={company} />)}</div>
     </main>
   );
 }
