b60d15b6f920d4384ef720be89989309c4b048f1
B3Kay <benjaminjekarlsson@gmail.com>
2024-10-10 17:54:06 +0200

    Fix typo

--- app/companies/[id]/page.tsx
+++ app/companies/[id]/page.tsx
@@ -1,30 +1,43 @@
+import Link from "next/link";
 import { baseUrl } from "../../../config";
 import { Company } from "../../../services/companies/types";
 import { notFound } from 'next/navigation';
+import { ChevronLeftIcon } from "lucide-react";
 
+
+type CompanySuccessful = {
+    company: Company;
+    error: null;
+}
+
+type CompanyError = {
+    company: null;
+    error: string;
+}
 
 const getCompany = async (id: number) => {
     const res = await fetch(`${baseUrl}/api/company?id=${id}`);
 
     if (!res.ok) {
-        console.warn("Unexpected response status:", res.status);
-        throw new Error("Failed to fetch data");
+        return { company: null, error: 'Company not found' } as CompanyError;
     }
-
     const data = await res.json();
-    return data as Company;
+    return { company: data, error: null } as CompanySuccessful;
 }
 
 export default async function CompanyPage({ params }: { params: { id: string } }) {
     const companyId = parseInt(params.id);
 
-    const company = await getCompany(companyId);
+    const response = await getCompany(companyId);
 
-    if (!company) {
+    if (response.error || !response.company) {
         notFound();
     }
 
     return <div>
-        <h1>{company.companyName}</h1>
+        <Link className="text-blue-500 hover:underline flex items-center mb-6" href="/">
+            <ChevronLeftIcon className="w-4 h-4 mr-2" />Go back
+        </Link>
+        <h1 className="text-2xl font-bold">{response.company.companyName}</h1>
     </div>
 }
