8c9f6c2d818c6b156e9e63882457da57915236c2
B3Kay <benjaminjekarlsson@gmail.com>
2024-10-10 15:08:17 +0200

    Image loading should not move ui

--- components/companies/CompanyList.tsx
+++ components/companies/CompanyList.tsx
@@ -9,13 +9,15 @@ interface CompanyListItemProps {
 export const CompanyListItem: React.FC<CompanyListItemProps> = ({ company }) => {
     return (
         <div className="company-card flex items-center my-4 border-b border-slate-200 pb-4">
-            <Image
-                className="max-h-10 max-w-10 w-auto mr-4"
-                src={company.iconUrl || company.logoLightUrl}
-                alt={`${company.companyName} logo`}
-                width={40}
-                height={40}
-            />
+            <div className="relative w-10 h-10 mr-4 flex justify-center items-center">
+                <Image
+                    className="max-h-10 max-w-10 w-auto object-contain"
+                    src={company.iconUrl || company.logoLightUrl}
+                    alt={`${company.companyName} logo`}
+                    width={40}
+                    height={40}
+                />
+            </div>
             <div className="flex-1">
                 <h3 className="m-0  text-md ">{company.displayName}</h3>
                 <p className="text-slate-500 text-sm line-clamp-1 max-w-[200px]">{company.description}</p>
